9305228ff78d22519c8500c41eb3006ef7f4562a
bettybettybettybeza <135344318+bettybettybettybeza@users.noreply.github.com>
2025-05-26 19:22:54 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}z
+
+resource "aws_s3_bucket" "beginner_bucket" {
+  bucket = "my-cloud-starterkit-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
+}
